924cde97c46136feb6a65582ea65d275dd4b3e53
saebom <boma91@gmail.com>
2021-04-21 04:35:41 +0900

    Add: route 내용 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import { Navbar, Nav, NavDropdown, Button, Jumbotron } from 'react-bootstrap';
+
 import Data from './data.js';
 import Product from './component/Product';
 import Detail from './component/Detail';
@@ -32,26 +33,26 @@ function App() {
         </Navbar.Collapse>
       </Navbar>
 
+      <Switch>  {/* Switch 안에는 모든 Route가 담겨있어야 한다(화면 내 router들이 switch되도록) */}
 
-      <Route exact path="/">
-        {/* jumbotron bootstrap */}
-        <Jumbotron className="background">
-          <h1>20% Season OFF</h1>
-          <p>
-            This is a simple hero unit, a simple jumbotron-style component for calling
-            extra attention to featured content or information.
-        </p>
-          <p>
-            <Button variant="primary">Learn more</Button>
-          </p>
-        </Jumbotron>
+        <Route exact path="/">
+          {/* jumbotron bootstrap */}
+          <Jumbotron className="background">
+            <h1>20% Season OFF</h1>
+            <p>
+              This site offer a biggest sale product in the Korea.
+              We hope you to enjoy here</p>
+            <p>
+              <Button variant="primary">Learn more</Button>
+            </p>
+          </Jumbotron>
 
-        {/* 상품표기(3등분) */}
-        <div className="container">
-          <div className="row">  {/* 총 12개의 column(기본)으로 나누겠다는 bootstrap 문법 */}
+          {/* 상품표기(3등분) */}
+          <div className="container">
+            <div className="row">  {/* 총 12개의 column(기본)으로 나누겠다는 bootstrap 문법 */}
 
-            {/* col-4 : 4개의 row로 나누겠다는 bootstrap 문법  / col-md-4 : 모바일에서 세로 1열 정렬 되게 만드는 bootstrap 문법 */}
-            {/* <div className="col-md-4"> 
+              {/* col-4 : 4개의 row로 나누겠다는 bootstrap 문법  / col-md-4 : 모바일에서 세로 1열 정렬 되게 만드는 bootstrap 문법 */}
+              {/* <div className="col-md-4"> 
             <img src="http://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
             <h4> {shoes[0].title} </h4>
             <p> {shoes[0].content} & {shoes[0].price} </p>
@@ -69,23 +70,29 @@ function App() {
             <p> {shoes[2].content} &  {shoes[2].price} </p>
           </div> */}
 
-            {
-              shoes.map((a, i) => {
-                return (
-                  <Product shoes={shoes[i]} i={i} />
-                )
-              })
-            }
+              {
+                shoes.map((a, i) => {
+                  return (
+                    <Product shoes={shoes[i]} i={i} key={i} />
+                  )
+                })
+              }
+
+            </div>
           </div>
-        </div>
-      </Route>
+
+        </Route>
 
 
-      <Route exact path="/detail">
-        <Detail />
-      </Route>
+        <Route exact path="/detail">
+          <Detail />
+        </Route>
 
+        <Route path="/:id">  {/* "/모든문자" 라는 경로를 의미 */}
+          <div>아무거나 적었을 때 이거 보여주세요</div>
+        </Route>
 
+      </Switch>
     </div>
   );
 }
